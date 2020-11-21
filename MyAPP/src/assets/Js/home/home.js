dde9a1fc5a81d2a9a59f1d3f36aa197f224b7c7f
songdengqiang <ai491154541@163.com>
2020-11-21 21:32:30 +0800

    大论文2

--- MyAPP/src/assets/Js/home/home.js
+++ MyAPP/src/assets/Js/home/home.js
@@ -58,6 +58,43 @@ const api = {
           data: data
         }]
     })
+  },
+  // 绘制饼图
+  drawPi: (container, data) => {
+    const PiGraph = echarts.init(document.getElementById(container));
+    PiGraph.setOption({
+      angleAxis: {
+        type: 'category',
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      },
+      radiusAxis: {
+      },
+      polar: {
+      },
+      series: [{
+        type: 'bar',
+        data: [1, 2, 3, 4, 3, 5, 1],
+        coordinateSystem: 'polar',
+        name: 'A',
+        stack: 'a'
+      }, {
+        type: 'bar',
+        data: [2, 4, 6, 1, 3, 2, 1],
+        coordinateSystem: 'polar',
+        name: 'B',
+        stack: 'a'
+      }, {
+        type: 'bar',
+        data: [1, 2, 3, 4, 1, 2, 5],
+        coordinateSystem: 'polar',
+        name: 'C',
+        stack: 'a'
+      }],
+      legend: {
+        show: true,
+        data: ['A', 'B', 'C']
+      }
+    })
   }
 }
 
